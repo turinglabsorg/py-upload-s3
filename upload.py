8d83433d0d9e9a9c45bcc879233956fbc125a662
turinglabs <sebastiano.cataudo@gmail.com>
2024-06-18 19:39:07 +0200

    basic s3 to upload script

---
+++ upload.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+from dotenv import load_dotenv 
+# loading variables from .env file
+load_dotenv() 
+
+# Define your AWS credentials
+aws_access_key_id = os.getenv('ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('SECRET_ACCESS_KEY')
+# Define the name of the S3 bucket and the file you want to upload
+bucket_name = os.getenv('BUCKET_NAME')
+file_name = 'test.txt'
+# Create a Boto3 S3 client
+s3 = boto3.client('s3',aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+# Upload the file to S3
+s3.upload_file(file_name, bucket_name, file_name)
+print(f'{file_name} uploaded successfully to {bucket_name}.')
